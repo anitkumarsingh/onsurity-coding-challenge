f8d4d48431732a47740b482abc7b9d5df2916896
Anit Kumar <anikumarsingh99@gmail.com>
2019-05-18 19:04:50 +0530

    completed second section

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import SelectCard from '../../Components/Card/Card';
+import BrandSelect from '../../Components/DropDown/BrandDropDwn';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const styles = theme =>({
   headerTitle:{
@@ -33,6 +37,7 @@ const styles = theme =>({
     display: 'flex',
     alignItems: 'stretch',
     // fontSize:'22px',
+    flexWrap:'wrap',
     '&&>div':{
       color: 'white',
       width: '100px',
@@ -43,21 +48,91 @@ const styles = theme =>({
    position:'relative',
    marginTop:'40%',
   },
-  // flexiContain:{
-  //   display: 'flex',
-  //   alignItems: 'stretch',
-  //   backgroundColor: '#f1f1f1',
+  flexiContain:{
+    display: 'flex',
+    flexWrap:'wrap',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    '&&>div':{
+      backgroundColor: 'white',
+      color: 'white',
+      width: '300px',
+      // marginBottom: '10px',
+      // height:'300px'
+    }
+  },
+  centeredBtn: {
+    textAlign: 'center',
+  },
+  button: {
+    margin: theme.spacing.unit,
+    backgroundColor:'#f04336',
+    boxShadow: '12px 12px 12px 12px rgba(225, 0, 0, 0.1)',
+    padding:'10px 30px 10px 30px',
+    fontSize:'1em'
+  },
+  row:{
+    display:'flex',
+    '&&:after':{
+      content:'',
+      display:'table',
+      clear:'both'
+    }
+  },
+  box:{
+    height : '200px',
+    color : 'white',
+    float: 'left',
+    width: '33.33%',
+    padding: '15px',
+    textAlign:'center'
+  },
+  spacer:{
+    margin:'90px 0 90px 0'
+  },
+  tellUsDeviceText:{
+    marginTop:'260px',
+    textAlign:'center'
+  },
+  personalizedTxtBox:{
+    marginTop:'233px',
+    textAlign:'center'
+  },
+  payNgetInsuredTxt:{
+    bottom:0,
+    textAlign:'center',
+    position:'relative'
+  },
+  secondSpacer:{
+    margin:'90px 0 0px 0',
+    background:'white',
+    color:'white'
+  },
+  top: {
+    marginTop: '10px',
+    fontSize: '1rem',
+  },
+  selectContent: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginTop: '-507px',
+    width: '41%',
+    // marginLeft: '3%',
+    backgroundColor: '#ffff',
+  },
+  ladyWithPhoneImg: {
+    height: 600,
+    // background:'url("/img/LadyWithPhone.png")',
+    backgroundSize: '700px',
+    backgroundPosition:'top right ',
+    backgroundRepeat:'no-repeat',
+  },
+  hideOnSmallDev:{
+    [theme.breakpoints.down('sm')]: {
+      visibility: 'hidden',
+    },
+  }
 
-  //   '&&>div':{
-  //     backgroundColor: 'DodgerBlue',
-  //     color: 'white',
-  //     width: '100px',
-  //     margin: '10px',
-  //     textAlign: 'center',
-  //     lineHeight: '75px',
-  //     fontSize: '30px'
-  //   }
-  // }
 })
 
 const Home = ({classes}) =>{
@@ -69,20 +144,72 @@ const Home = ({classes}) =>{
       <Typography component="h6" variant="h6" className={classes.subTitle}>
        Simple. Smart. Affordable
       </Typography>
-      {/* <div className={classes.imgContainer}>
-        <div className={classes.media}>
-             <SelectCard/>
-        </div>
-      </div> */}
       <div className={classes.flexContainer}>
-      <div style={{flexGrow:'5'}} className={classes.selectCard}> <SelectCard/></div>
-      <div style={{flexGrow:'8'}} className={classes.imgContainer}></div>
+          <div style={{flexGrow:'5'}} className={classes.selectCard}>
+            <SelectCard/>
+          </div>
+          <div style={{flexGrow:'8'}} className={classes.imgContainer}></div>
       </div>
-      {/* <div className={classes.flexiContain}>
-         <div style={{flexGrow:'4'}}>1</div>
-         <div style={{flexGrow:'4'}}>2</div>
-         <div style={{flexGrow:'4'}}>3</div>
+      <Typography component="h1" variant="h4" className={classes.headerTitle}>
+       {`It's Easier than You Think`}
+      </Typography>
+      <div className={classes.spacer}></div>
+      <div className={classes.flexiContain}>
+         <div><BrandSelect/>
+            <div className={classes.tellUsDeviceText}>
+                  <Typography variant="subtitle1" component="h2">
+                    Tell us about your device
+                   </Typography>
+              </div>
+         </div>
+         <div className={classes.centeredBtn}>
+             <Button variant="contained" color="secondary"className={classes.button}>
+               Select
+             </Button>
+              <div className={classes.personalizedTxtBox}>
+                    <Typography variant="subtitle1" component="h2">
+                      Select a personalized plan
+                     </Typography>
+                </div>
+         </div>
+         <div className={classes.centeredBtn}>
+            <img
+              src="/img/InsuredGIF.png"
+              alt="Onsurity"
+              width={'auto'}
+              height={290}
+              />
+              <div className={classes.payNgetInsuredTxt}>
+                  <Typography variant="subtitle1" component="h2">
+                   Pay and get insured instantly
+                  </Typography>
+              </div>
+         </div>
+      </div>
+      <div className={classes.secondSpacer}>1</div>
+      {/* <div className={classes.row}>
+         <div className={classes.box}>
+            <Typography variant="subtitle1" component="h2">Tell us about your device</Typography>
+         </div>
+         <div className={classes.box}>
+            <Typography variant="subtitle1" component="h2">Select a personalized plan</Typography>
+         </div>
+         <div className={classes.box}>
+            <Typography variant="subtitle1" component="h2">Pay and get insured instantly</Typography>
+         </div>
       </div> */}
+      <Grid container>
+            <Grid item xs={12} sm={12} className={classes.hideOnSmallDev}>
+              <CardMedia
+                className={classes.ladyWithPhoneImg}
+                image="/img/LadyWithPhone.png"
+                title="about us"
+              />
+            </Grid>
+            <div className={classes.selectContent}>
+                <SelectCard/>
+            </div>
+          </Grid>
     </div>
   )
 }
